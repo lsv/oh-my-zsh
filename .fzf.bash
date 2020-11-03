5774884062452ec257fa542e14fcb7a2ee3d7e02
Martin Aarhof <martin.aarhof@gmail.com>
2020-11-03 11:20:02 +0100

    update

--- .fzf.bash
+++ .fzf.bash
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/lsv/.oh-my-zsh/fzf/bin* ]]; then
-  export PATH="$PATH:/home/lsv/.oh-my-zsh/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.oh-my-zsh/fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.oh-my-zsh/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/lsv/.oh-my-zsh/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.oh-my-zsh/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/lsv/.oh-my-zsh/fzf/shell/key-bindings.bash"
+source "$HOME/.oh-my-zsh/fzf/shell/key-bindings.bash"
 
--- install.sh
+++ install.sh
@@ -10,12 +10,10 @@ chsh -s /bin/zsh
 
 if [ ! -d /usr/local/bin/composer ]; then
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-	php -r "if (hash_file('sha384', 'composer-setup.php') === 'baf1608c33254d00611ac1705c1d9958c817a1a33bce370c0595974b342601bd80b92a3f46067da89e3b06bff421f182') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 	php composer-setup.php --install-dir=/usr/local/bin
 	php -r "unlink('composer-setup.php');"
 	sudo ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
 	sudo ln -s /usr/local/bin/composer.phar /usr/local/bin/c
-	composer global require hirak/prestissimo
 fi
 
 if [ ! -d ~/.config/tilda ]; then
